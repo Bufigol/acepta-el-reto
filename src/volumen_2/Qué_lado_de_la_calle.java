c2652f9e9cb307755ecb47608d67163087c23a10
Felipe Andrés Vásquez Tort <felipevasquez.tort@gmail.com>
2015-10-23 13:16:03 +0200

    Agregado un nuevo problema resuelto que ha sido aceptado por el juez virtual. El problema corresponde al numero 217:¿Qué lado de la calle?

---
+++ src/volumen_2/Qué_lado_de_la_calle.java
@@ -0,0 +1,18 @@
+package volumen_2;
+
+public class Qué_lado_de_la_calle {
+	static java.util.Scanner in;
+
+	public static void main(String[] args) {
+		in = new java.util.Scanner(System.in);
+		Integer n = in.nextInt();
+		while (n > 0) {
+			if (n % 2 == 0) {
+				System.out.println("DERECHA");
+			} else {
+				System.out.println("IZQUIERDA");
+			}
+			n = in.nextInt();
+		}
+	}
+}
